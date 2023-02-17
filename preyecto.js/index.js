31c52d146da41739346449ccf4505958877151d7
yordy <yordy.olivaresrosales@hotmail.com>
2023-02-17 13:47:34 -0600

    commit inicial

---
+++ preyecto.js/index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy felíz"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito felíz"
+
+    console.log(a)
+}
